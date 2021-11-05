4e1e2f0dd3d0b54a919f90c290e3422fd019ee21
rodrigo051 <91332304+rodrigo051@users.noreply.github.com>
2021-11-05 18:05:57 +0100

    Bootstrap hinzugefügt

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,23 @@
-print("Hello World")
+from flask import Flask
+from flask import render_template
+
+app = Flask ("Bootstrap_Versuch")
+
+
+@app.route("/")
+def start():
+    name = False
+    cards = [
+        {"titel": "Card 0", "inhalt": "Blubber"},
+        {"titel": "Card 1", "inhalt": "Bla"},
+        {"titel": "Card 2", "inhalt": "Käsekuchen"},
+        {"titel": "Card 3", "inhalt": "Sülze"},
+    ]
+    return render_template("index.html", name=name, cards=cards)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
+
+
