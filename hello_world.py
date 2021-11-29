cf5f9e98da520b1bb08430ada2e7dcb41932485a
rodrigo051 <91332304+rodrigo051@users.noreply.github.com>
2021-11-29 14:35:24 +0100

    Änderungen am Eingabeformular und Dictionary für Bergauswahl

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,42 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-import region_dict
+from region_dict import Frastanz
 
 
-app = Flask ("Berge")
+
+app = Flask ("Bergapp")
 
 # Code für Ausgangsseite mit Eingabe mithilfe der Website http://raspitips.de/flask-webapps-mit-python-erstellen/ erstellt
 @app.route("/", methods=["GET", "POST"])
 def formular_eingabe():
     return render_template("index.html")
+
+dict = Frastanz
+
+@app.route("/berge", methods=["GET", "POST"])
+def berge_filtern():
+    if request.method == "POST":
+        ort = request.form["ausgangsort"]
+        schwierigkeit = request.form["schwierigkeitslevel"]
+        anreise = request.form["anreisemittel"]
+
+        if ort == "Frastanz" and schwierigkeit == "leicht" and anreise == "Auto":
+            for key, values in dict.items():
+                for value in values:
+                    berg = key
+                    return berg
+
+        else:
+            error = "Deine Eingabe hat irgend einen Fehler"
+            return error
+
+
+
+
+#    return render_template("berge.html")
+
+
 
 
 if __name__ == "__main__":
--- region_dict.py
+++ region_dict.py
@@ -1,63 +1,15 @@
 
-#Dictionary für die Auflistung der Berge nach Region
-berge_region = {
-    'arlberg': {'berge': {'Rote Wand', 'Trittkopf', 'Roggelskopf', 'Saladinaspitze', 'Spullersee'}},
+#Dictionary für Bergauswahl
 
-    'brandnertal': {'berge': {'Loischkopf', 'Mondspitze', 'Schillerkopf', 'Fundelkopf', 'Schesaplana'}},
-
-    'bregenzerwald': {'berge': {'Hochhäderich', 'Hoher Freschen', 'Kanisfluh', 'Diedamskopf', 'Braunarlspitze'}},
-
-    'großes walsertal': {'berge': {'Tälispitze', 'Löffelspitze', 'Gerenspitze', 'Wangspitze', 'Zafernhorn'}},
-
-    'kleinwalsertal': {'berge': {'Hoher Ifen', 'Grünhorn', 'Widderstein', 'Geißhorn', 'Hammerspitze'}},
-
-    'klostertal': {'berge': {'Roggelskopf', 'Itonskopf', 'Muttjöchle', 'Burtschakopf', 'Kaltenberg Hütte'}},
-
-    'laternsertal': {'berge': {'Aussichtspunkt Säntisblick', 'Damülser Mittagsspitze', 'Mondspitze', 'Hoher Freschen', 'Glatthorn'}},
-
-    'montafon': {'berge': {'Saulakopf', 'Drei Türme', 'Tschaggunser Mittagsspitze', 'Madrisella', 'Kreuzjoch'}},
-
-    'rheintal': {'berge': {'Pfänder', 'Alpkopf', 'Drei Schwestern', 'Schönberg', 'Dornbirner First'}},
-
-    'walgau': {'berge': {'Drei Schwestern', 'Gurtisspitze', 'Hohe Köpf', 'Hochgerach', 'Naafkopf'}},
+Frastanz = {
+    "Gurtisspitze": {"Schwierigkeit": "leicht", "Anreise": "Auto"},
+    "Hohe Köpfe": {"Schwierigkeit": "mittel", "Anreise": "Auto"},
+    "Drei Schwestern": {"Schwierigkeit": "schwer", "Anreise": "Auto"},
+    "Zeawas Heil Spitz": {"Schwierigkeit": "leicht", "Anreise": "Öffentliche Verkehrsmittel"},
+    "Goppaschrofa": {"Schwierigkeit": "mittel", "Anreise": "Öffentliche Verkehrsmittel"},
+    "Hochgerach": {"Schwierigkeit": "schwer", "Anreise": "Öffentliche Verkehrsmittel"}
 }
 
-#Dictionary für die Auflistung der Orte in der Region
-orte_region = {
-    'arlberg': {'orte': {'Lech'}},
-
-    'brandnertal': {'orte': {'Brand', 'Bürserberg'}},
-
-    'bregenzerwald': {'orte': {'Alberschwende', 'Andelsbuch', 'Au', 'Bezau', 'Bizau', 'Damüls', 'Doren', 'Egg',
-                               'Hittisau', 'Krumbach', 'Langen bei Bregenz', 'Langenegg', 'Lingenau', 'Mellau',
-                               'Reuthe', 'Riefensberg', 'Schnepfau', 'Schoppernau', 'Schröcken', 'Schwarzenberg',
-                               'Sibratsgfäll', 'Sulzberg', 'Warth'}},
-
-    'großes walsertal': {'orte': {'Fontanella', 'Raggal', 'Sonntag', 'St. Gerold', 'Thüringerberg', 'Blons'}},
-
-    'kleinwalsertal': {'orte': {'Mittelberg'}},
-
-    'klostertal': {'orte': {'Dalaas', 'Innerbraz', 'Klösterle'}},
-
-    'laternsertal': {'orte': {'Laterns'}},
-
-    'leiblachtal': {'orte': {'Eichenberg', 'Hörbranz', 'Hohenweiler', 'Lochau', 'Möggers'}},
-
-    'montafon': {'orte': {'Bartholomäberg', 'Gaschurn', 'Lorüns', 'Schruns', 'Silbertal', 'St. Anton im Montafon',
-                          'St.Gallenkirch', 'Stallehr', 'Tschagguns', 'Vandans'}},
-
-    'rheintal': {'orte': {'Altach', 'Bildstein', 'Bregenz', 'Buch', 'Dornbirn', 'Feldkirch', 'Fraxern', 'Fußach',
-                          'Gaißau', 'Götzis', 'Hard', 'Höchst', 'Hohenems', 'Kennelbach', 'Klaus', 'Koblach',
-                          'Lauterach', 'Mäder', 'Meiningen', 'Rankweil', 'Röthis', 'Schwarzach', 'Sulz', 'Übersaxen',
-                          'Viktorsberg', 'Weiler', 'Wolfurt', 'Zwischenwasser'}},
-
-    'walgau': {'orte': {'Bludenz', 'Bludesch', 'Bürs', 'Düns', 'Dünserberg', 'Frastanz', 'Göfis', 'Ludesch', 'Nenzing',
-                        'Nüziders', 'Röns', 'Satteins', 'Schlins', 'Schnifis', 'Thüringen'}},
-}
-
-#zusammengefügtes Dictionary aus Berge + Orte in Liste speichern
-
-region_liste = [berge_region, orte_region]
 
 
 
